9a7cbee2763ee863972de8dd9a4576fc191506d6
Brianna Hickerson <briannahickerson@Briannas-MacBook-Pro.local>
2022-10-25 08:08:54 -0700

    finished assignment

---
+++ main.js
@@ -0,0 +1,25 @@
+let para = document.querySelector('p');
+para.style.color = "lightblue";
+
+let head = document.querySelector('h1');
+head.style.fontSize = "48px";
+
+para.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
+
+let item13 = document.querySelector("#item-13");
+item13.style.opacity = "0.5";
+
+let item3 = document.querySelector("#item-3");
+item3.innerText = "I say, Hi!";
+
+
+let myImage = document.querySelector("img");
+myImage.src ="https://residentevilpodcast.com/wp-content/uploads/2021/08/leonskennedy.jpg";
+myImage.style.height = "300px"
+
+let myImage2 = document.querySelector("img2");
+myImage2.style.height = "300px"
+
+let item16 =document.querySelector("#item-16");
+item16.className = "item"
+item16.innerText = "Wont get fooled again."
